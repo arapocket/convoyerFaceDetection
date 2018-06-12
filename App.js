a58be5b42def52cdbd17be9181e0a29ffd2f3ca0
arapocket <arakazaryan@me.com>
2018-06-12 16:51:07 -0700

    quick save

--- App.js
+++ App.js
@@ -1,58 +1,84 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+'use strict';
+
+import './ReactotronConfig'
 
 import React, { Component } from 'react';
 import {
-  Platform,
+  AppRegistry,
   StyleSheet,
-  Text,
-  View
+  View,
+  TouchableHighlight,
+  StatusBar,
+  Text
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
+import { Navigation } from 'react-native-navigation';
+
+import Icon from 'react-native-vector-icons/Ionicons';
+import BackgroundGeolocation from 'react-native-background-geolocation';
+
+global.BackgroundGeolocation = BackgroundGeolocation;
+
+import Config from './components/config';
+import HomeView from './components/screens/HomeView';
+import IncidentView from './components/screens/IncidentView';
+import CameraView from './components/screens/CameraView';
+import VideoView from './components/screens/VideoView';
+import ChatView from './components/screens/ChatView';
+import LoginView from './components/screens/LoginView';
+import OptionsView from './components/screens/OptionsView';
+import QRView from './components/screens/QRView';
+
+
+var startPage = 'convoyer.LoginView';
+console.disableYellowBox = true;
+
+
+export default class Application extends Component {
+
+
+
+  //with the navigation api, none of this stuff is being called it seems...
+
+  //   componentDidMount() {
+
+  //     StatusBar.setBarStyle('default');
+  //     console.ignoredYellowBox = ['Remote debugger'];
+
+  //   }
+  //   render() {
+  //     return (
+  //       <View  >
+  //         <LoginView />
+  //       </View>
+  //     );
+  //   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+
+
+Navigation.registerComponent('convoyer.HomeView', () => HomeView);
+Navigation.registerComponent('convoyer.CameraView', () => CameraView);
+Navigation.registerComponent('convoyer.VideoView', () => VideoView);
+Navigation.registerComponent('convoyer.IncidentView', () => IncidentView);
+Navigation.registerComponent('convoyer.ChatView', () => ChatView);
+Navigation.registerComponent('convoyer.LoginView', () => LoginView);
+Navigation.registerComponent('convoyer.OptionsView', () => OptionsView);
+Navigation.registerComponent('convoyer.QRView', () => QRView);
+
+
+Navigation.startSingleScreenApp({
+  screen: {
+    screen: startPage,
+    title: 'CONVOYER',
+    navigatorStyle: {
+      navBarTextColor: Config.colors.white
+    }
+  }
 });
+
+
+
+
+
